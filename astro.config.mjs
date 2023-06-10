308cc7ed786871ca06fa250e77ffe1dd0615d2b2
Alvaro <alvgaona@gmail.com>
2023-06-10 00:38:01 -0300

    Use edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
 
 
 // https://astro.build/config
