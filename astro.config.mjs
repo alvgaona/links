d74a9aeca26e47b1c51fa0a74c61d41be53c736a
Alvaro <alvgaona@gmail.com>
2023-06-10 00:33:13 -0300

    Use experimental assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  experimental: {
+    assets: true
+  }
 });
